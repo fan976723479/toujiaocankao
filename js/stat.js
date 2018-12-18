df4c2e7cac51e1a9c073db51d1ad860d9619afb8
fan976723479 <976723479@qq.com>
2018-12-18 09:56:14 +0800

    news

---
+++ js/stat.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?d1ba3948bd4f1aaa00974cc0af940adb";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
